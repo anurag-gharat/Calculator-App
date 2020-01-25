7eee42fccef618711d6825a898569801e922a9db
anurag gharat <anuraggharat55@gmail.com>
2020-01-25 13:20:53 +0530

    "Calculator layout done"

--- js/app.js
+++ js/app.js
@@ -5,19 +5,3 @@ $(function(){
             }, 10);
 
 });
-//start of calculation js
-var num1=document.getElementById('num1');
-var num2=document.getElementById('num2');
-function addition(){
-    var num1=document.getElementById('num1');
-    var num2=document.getElementById('num2');
-    if (num1==null || num2==null) {
-        alert('No Null values allowed');
-        
-    } 
-    else 
-    {
-     document.getElementById('answer').innerHTML="num1+num2";   
-    }
-
-}
