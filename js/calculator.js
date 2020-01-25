d60e89a9b3c243881e364f71ed423d734a33013a
anurag gharat <anuraggharat55@gmail.com>
2020-01-25 16:06:04 +0530

    "all working normal calculator"

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,16 @@
 function insert(num){
     document.form.textview.value=document.form.textview.value+num; 
 }
+function eqaulsto(){
+    var equation=document.form.textview.value;
+    if(equation){
+        document.form.textview.value=eval(document.form.textview.value);
+    }
+}
+function clean(){
+    document.form.textview.value="";
+}
+function backspace(){
+    var num = document.form.textview.value;
+    document.form.textview.value=num.substring(0,num.length-1);
+}
